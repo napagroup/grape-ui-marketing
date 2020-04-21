8ed11c12c7a93f06091361d328db9ae113a94d60
ryanespin <ryanespin@Ryans-MBP.fios-router.home>
2020-04-21 17:07:00 -0400

    All fixed: * 🔴Added support fot SVGs * 💫Updated start script * 🔗Updated links * 🔢Now passing in version to header * 📱Cleaned up landing page for mobile * 👩‍🏫Example code is cleaner * 🤷‍♀️404 updated

--- src/containers/HomePage/InstallUsageContainer.js
+++ src/containers/HomePage/InstallUsageContainer.js
@@ -4,9 +4,7 @@ import {
   Button,
   Flex,
   Header,
-  Link as GrapeLink,
   Paragraph,
-  Text,
 } from 'grape-ui-react';
 import React from 'react';
 import styled from 'styled-components';
@@ -20,39 +18,29 @@ const GrayBox = styled(Box)`
 `;
 
 GrayBox.defaultProps = {
+  maxWidth: 480,
   mb: [2, 0],
-  mx: [null, 1],
+  mx: 'auto',
   px: [1, 2, 3, 4],
   py: [1, 2, 3],
-  width: [1, 1 / 2],
+  width: 1,
 };
-
-const installStepOne = 'npm install styled-components';
-const installStepTwo = 'npm install grape-ui-react';
 
 const usageExample = `
-import React from ‘react’;
-import { ThemeProvider } from ‘styled-components’;
+import React from 'react';
 import { Paragraph } from 'grape-ui-react';
-
-const appTheme = {
-  fonts: {
-    base: '"Arial", sans-serif',
-  },
-  space: [0, 8, 16, 32, 64, 128],
-};
 
 const App = () => {
   return (
     <div className="App">
-      <ThemeProvider theme={appTheme}>
-        <Paragraph>grape-ui is Groovy</Paragraph>
-      </ThemeProvider>
+      <Paragraph>
+        grape-ui is Groovy
+      </Paragraph>
     </div>
   )
 };
 
-default export App;
+export default App;
 `;
 
 const InstallUsageContainer = () => (
@@ -66,30 +54,26 @@ const InstallUsageContainer = () => (
       width={[19 / 20, null, null, 1]}
     >
       <GrayBox>
-        <Header.h2 textAlign="center">Installation</Header.h2>
+        <Header.h2 textAlign="center">
+          Installation
+        </Header.h2>
         <Paragraph>
-          <Text fontWeight="bold">Step 1:</Text>
-          {' Install '}
-          <GrapeLink href={urlRoutes.externalStyledComponents}>styled-components</GrapeLink>
-          {' via npm'}
+          Install grape-ui-react via npm
         </Paragraph>
-        <CodeBlock code={installStepOne} language="bash" />
-        <Paragraph>
-          <Text fontWeight="bold">Step 2:</Text>
-          {' Install grape-ui via npm'}
-        </Paragraph>
-        <CodeBlock code={installStepTwo} language="bash" />
-        <hr />
-        <Link to={urlRoutes.docsPage}>
-          <Button color="black" width={1}>
-            Install Docs
-          </Button>
-        </Link>
-      </GrayBox>
-      <GrayBox>
-        <Header.h2 textAlign="center">Usage</Header.h2>
+        <CodeBlock
+          code="npm i grape-ui-react"
+          language="bash"
+        />
+        <Box py={[1, 2, 3, 4]}>
+          <hr />
+        </Box>
+        <Header.h2 textAlign="center">
+          Usage
+        </Header.h2>
         <CodeBlock code={usageExample} />
-        <hr />
+        <Box pt={[1, 2, 3, 4]}>
+          <hr />
+        </Box>
         <Link to={urlRoutes.docsPage}>
           <Button color="black" width={1}>
             Learn More
