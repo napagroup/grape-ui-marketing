01231c2a3b4de12c6351dbb27ac369c666c9bcf7
ryanespin <ryanespin@Ryans-MacBook-Pro.local>
2020-06-17 09:57:38 -0400

    Updated grape-ui-react, added in a Card.

--- src/containers/HomePage/InstallUsageContainer.js
+++ src/containers/HomePage/InstallUsageContainer.js
@@ -2,24 +2,11 @@ import { Link } from 'gatsby';
 import {
   Box,
   Button,
+  Card,
   CodeBlock,
-  Flex,
-  Header,
-  Paragraph,
 } from 'grape-ui-react';
 import React from 'react';
-import styled from 'styled-components';
 import urlRoutes from '../../components/constants';
-
-const GrayBox = styled(Box)``;
-
-GrayBox.defaultProps = {
-  boxSizing: 'border-box',
-  maxWidth: 480,
-  mb: [2, 0],
-  mx: 'auto',
-  width: 1,
-};
 
 const usageExample = `
 import React from 'react';
@@ -39,43 +26,46 @@ export default App;
 `;
 
 const InstallUsageContainer = () => (
-  <Box py={[2, 3, 4, 5]}>
-    <Flex
-      alignItems="flex-start"
-      flexDirection={['column', 'row']}
-      maxWidth={960}
-      mx="auto"
-      style={{ boxSizing: 'border-box' }}
-      width={[19 / 20, null, null, 1]}
-    >
-      <GrayBox>
-        <Header.h2>
-          Installation
-        </Header.h2>
-        <Paragraph>
-          Install grape-ui-react via npm
-        </Paragraph>
+  <Box py={[3, 4, 5]}>
+    <Card
+      cardBody={(
         <CodeBlock
           codeString="npm i grape-ui-react"
           language="bash"
           my="initial"
-          mb={4}
         />
-        <Header.h2>
-          Usage
-        </Header.h2>
-        <CodeBlock
-          codeString={usageExample}
-          my="initial"
-          mb={4}
-        />
+      )}
+      cardSubtitle="Install grape-ui-react via npm"
+      cardTitle="Installation"
+      maxWidth={400}
+      mx="auto"
+      width={1}
+    />
+    <Card
+      cardActions={(
         <Link to={urlRoutes.docsPage}>
-          <Button color="brandPrimary" width={1}>
+          <Button
+            bg="brandPrimary"
+            bgActiveColor="brandPrimary.dark"
+            bgHoverColor="brandPrimary.light"
+            color="white"
+            width={1}
+          >
             Learn More
           </Button>
         </Link>
-      </GrayBox>
-    </Flex>
+      )}
+      cardBody={(
+        <CodeBlock
+          codeString={usageExample}
+          my="initial"
+        />
+      )}
+      cardTitle="Usage"
+      maxWidth={400}
+      mx="auto"
+      width={1}
+    />
   </Box>
 );
 
